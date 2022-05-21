15b7711575eef4a6ab2d5079a256a56650d8d0e2
WesleyBLDC <wezzdc@gmail.com>
2022-05-21 13:23:17 +0800

    init prisma/supa

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,64 +1,69 @@
 import {
-        Flex, Box, Button, 
-        FormControl, Input, FormLabel,
-        Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter,
-        useDisclosure,
-        InputLeftElement,
-        InputGroup
-      } from "@chakra-ui/react";
-import {useRef} from 'react'
-import {MdEmail, MdPassword} from 'react-icons/md'
+  Flex,
+  Box,
+  Button,
+  FormControl,
+  Input,
+  FormLabel,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  InputLeftElement,
+  InputGroup,
+} from "@chakra-ui/react";
+import { useRef } from "react";
+import { MdEmail, MdPassword } from "react-icons/md";
 
-function Navbar() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const initialRef = useRef()
-  const finalRef = useRef()
-  
+  const initialRef = useRef();
+  const finalRef = useRef();
+
   return (
     <Box h="50px" w="100%">
       <Flex padding={5} float="right">
         <Button onClick={onOpen}>Login / Sign up</Button>
       </Flex>
-    
+
       <Modal
         initialFocusRef={initialRef}
         finalFocusRef={finalRef}
         isOpen={isOpen}
         onClose={onClose}
-        >
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader> Login</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody pb={6} >
-              <FormControl>
-                <InputGroup>
-                  <InputLeftElement children={<MdEmail />}/>
-                  <Input ref={initialRef} placeholder='Email'/>
-                </InputGroup>
-                
-              </FormControl>
-
-              <FormControl mt={4}>
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader> Login</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
               <InputGroup>
-                  <InputLeftElement children={<MdPassword />}/>
-                  <Input ref={initialRef} placeholder='Password' />
-                </InputGroup>
-                
-                
-              </FormControl>
-            </ModalBody>
+                <InputLeftElement children={<MdEmail />} />
+                <Input ref={initialRef} placeholder="Email" />
+              </InputGroup>
+            </FormControl>
 
-            <ModalFooter>
-              <Button mr={3}>Create</Button>
-              <Button>Login</Button>
-            </ModalFooter>
+            <FormControl mt={4}>
+              <InputGroup>
+                <InputLeftElement children={<MdPassword />} />
+                <Input ref={initialRef} placeholder="Password" />
+              </InputGroup>
+            </FormControl>
+          </ModalBody>
 
-          </ModalContent>
+          <ModalFooter>
+            <Button mr={3}>Create</Button>
+            <Button>Login</Button>
+          </ModalFooter>
+        </ModalContent>
       </Modal>
     </Box>
-
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,13 +12,29 @@ import {
 } from "@chakra-ui/react";
 import Head from "next/head";
 import Image from "next/image";
+import { PrismaClient } from "@prisma/client";
 import styles from "../styles/Home.module.css";
 import "reset-css";
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
 
-import DateTime from "../components/DateTime"
+import DateTime from "../components/DateTime";
 import Sidebar from "../components/sidebar";
 
+import Layout from "../components/Layout";
+import Grid from "../components/Grid";
+
+const prisma = new PrismaClient();
+
+export async function getServerSideProps() {
+  // Get all homes
+  const tasks = await prisma.task.findMany();
+  // Pass the data to the Home page
+  return {
+    props: {
+      tasks: JSON.parse(JSON.stringify(tasks)),
+    },
+  };
+}
 
 const IndexPage = () => {
   return (
@@ -31,14 +47,16 @@ const IndexPage = () => {
             <Box w="150px" h="100%" bg="purple.100">
               <Sidebar />
             </Box>
-            <Box w="100%" >
+            <Box w="100%">
               <DateTime />
+              <div className="mt-8">
+                <Grid homes={homes} />
+              </div>
             </Box>
           </Flex>
         </Box>
       </VStack>
     </Box>
-
   );
 };
 
