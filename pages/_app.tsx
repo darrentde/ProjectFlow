9bcfc5dd1f49c0b8615d4502121cc31c243dff2a
WesleyBLDC <wezzdc@gmail.com>
2022-05-22 00:09:12 +0800

    prisma/supa 1.2 redo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,15 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { AppProps } from "next/app";
 import { SessionProvider as AuthProvider } from "next-auth/react";
-import { Toaster } from "react-hot-toast";
-
 import theme from "../src/theme";
 import "../src/theme/styles.css";
-import { SessionProvider } from "next-auth/react";
-
-// const MyApp = ({ Component, pageProps }: AppProps) => {
-//   return (
-//     <ChakraProvider theme={theme}>
-//       <Component {...pageProps} />
-//     </ChakraProvider>
-//   );
-// };
-
-// export default MyApp;
 
 const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
-    <SessionProvider session={session}>
-      <ChakraProvider theme={theme}>
+    <ChakraProvider theme={theme}>
+      <AuthProvider session={session}>
         <Component {...pageProps} />
-      </ChakraProvider>
-    </SessionProvider>
+      </AuthProvider>
+    </ChakraProvider>
   );
 };
 
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,6 +7,7 @@ import Handlebars from "handlebars";
 import { readFileSync } from "fs";
 import path from "path";
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "../../../lib/prisma";
 
 // const transporter = nodemailer.createTransport({
@@ -38,6 +39,12 @@ import prisma from "../../../lib/prisma";
 // };
 
 export default NextAuth({
+  pages: {
+    signIn: "/",
+    signOut: "/",
+    error: "/",
+    verifyRequest: "/",
+  },
   providers: [
     // EmailProvider({
     //   server: {
@@ -51,6 +58,35 @@ export default NextAuth({
     //   from: process.env.EMAIL_FROM,
     //   maxAge: 10 * 60,
     // }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: {
+          label: "Email",
+          type: "email",
+          placeholder: "jsmith@test.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: 1, email: "hello@test.com", password: "12345" };
+        // database lookiup
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        }
+        // If you return null then an error will be displayed advising the user to check their details.
+        return null;
+
+        // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+      },
+    }),
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
