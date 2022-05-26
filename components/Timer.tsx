4f6fed5180a0510e48e8dffe355d8428fac95d7c
darrentde <darrent2599@gmail.com>
2022-05-26 16:40:46 +0800

    Skeleton code for timer

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,6 +1,46 @@
+import { Box,Flex,Text } from "@chakra-ui/layout";
+import { Button, IconButton } from "@chakra-ui/react";
+import{FiSettings} from "react-icons/fi"
+import {MdRefresh} from "react-icons/md"
+import {IoMdPlay} from "react-icons/io"
+
 function Timer() {
     return(
-    <h1>Yes</h1>
+    <Box position='absolute' top='300px' left ='600px' bg='white' border ='0.1rem solid black' 
+    width ='15rem' height='auto' borderRadius='10px'>
+        {/* <Box borderBottom='0.1rem' borderBottomColor='black' borderBottomStyle='ridge'> */}
+        <Box>
+            <Text className="Header" margin='10px'> Pomodoro </Text>
+        </Box>
+        <Box>
+            <Flex flexDirection="row" justifyContent="space-evenly">
+                 <Text fontSize='30px'>00:00</Text>
+                 <Flex flexDirection='column' gap='10px'>
+                    {/* <Button>Start</Button> */}
+                    <IconButton 
+                    icon={<IoMdPlay />} 
+                    aria-label={"Plays"}   
+                    variant='link'       
+                />
+                    <IconButton 
+                    icon={<MdRefresh />} 
+                    aria-label={"Refresh"}   
+                    variant='link'       
+                />
+
+                 </Flex>
+            </Flex>
+        </Box>
+        <Box>
+            <Flex position = 'relative' justifyContent='flex-end' margin='10px'>
+                <IconButton 
+                    icon={<FiSettings />} 
+                    aria-label={"Settings"}     
+                    variant='link'              
+                />
+            </Flex>
+        </Box>
+    </Box>
     )
 }
 
--- components/Todo.tsx
+++ components/Todo.tsx
@@ -2,7 +2,7 @@ import {Box} from "@chakra-ui/react"
 
 function Todo() {
     return(
-    <Box position='fixed' top ='56' left='400' bg='white' border ='0.1rem solid black' 
+    <Box position='absolute' top='200px' left ='160px' bg='white' border ='0.1rem solid black' 
 		width ='15rem' height='10rem'>
 			<Box borderBottom='0.1rem' borderBottomColor='black' borderBottomStyle='ridge'>
 				Header				
