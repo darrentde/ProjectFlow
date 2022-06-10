a367aa24ba3973e2a53225a1f52feae4afc465c3
darrentde <darrent2599@gmail.com>
2022-06-10 12:35:30 +0800

    Added break and session transition Added sound when alarm ends

--- components/TimerCountdown.tsx
+++ components/TimerCountdown.tsx
@@ -1,44 +1,72 @@
-import React, { useEffect } from "react";
-import { Text } from "@chakra-ui/layout";
+/* eslint-disable global-require */
+/* eslint-disable jsx-a11y/media-has-caption */
+import React, { useEffect, useRef } from "react";
+import { Box, Text } from "@chakra-ui/layout";
 
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../redux/Store";
-import { decrementTimerValue } from "../redux/TimerSlice";
+import {
+  decrementTimerValue,
+  stopTimer,
+  updateTimerValue,
+  toggleLabel,
+  resetTimer,
+} from "../redux/TimerSlice";
 
 export const TimerCountdown = () => {
   const timerValue = useSelector((state: RootState) => state.timer.timerValue);
   const isRunning = useSelector((state: RootState) => state.timer.isRunning);
+  const timerLabel = useSelector((state: RootState) => state.timer.timerLabel);
+
+  const sessionValue = useSelector(
+    (state: RootState) => state.timer.sessionValue
+  );
+  const breakValue = useSelector((state: RootState) => state.timer.breakValue);
+
   const dispatch = useDispatch();
 
   const minutes = Math.floor(timerValue / 60);
   const seconds = timerValue - minutes * 60;
-
   const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
   const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  const notificationSound = require("../public/assets/sound.mp3");
+  const notificationRef = useRef(null);
 
   useEffect(() => {
     if (isRunning) {
       const interval = setInterval(() => {
-        dispatch(decrementTimerValue());
-        // if(seconds === 0) {
-        //     if(minutes !== 0) {
-        //         setSeconds(59)
-        //         setMinutes(minutes - 1)
-        //     } else {
-        //         setSeconds(seconds)
-        //         setMinutes(minutes)
-        //     }
-        // } else {
-        //     setSeconds(seconds - 1)
-        // }
+        if (timerValue === 0) {
+          notificationRef.current.play();
+          if (timerLabel === "Session") {
+            dispatch(updateTimerValue(breakValue));
+          } else if (timerLabel === "Break") {
+            dispatch(resetTimer);
+            dispatch(updateTimerValue(sessionValue));
+          }
+
+          dispatch(toggleLabel(timerLabel));
+          dispatch(stopTimer());
+        } else {
+          dispatch(decrementTimerValue());
+        }
       }, 1000);
       return () => clearInterval(interval);
     }
-  }, [dispatch, isRunning]);
+  });
 
   return (
-    <Text fontSize="3.5rem">
-      {timerMinutes}:{timerSeconds}
-    </Text>
+    <Box>
+      <Text> {timerLabel}</Text>
+      <Text fontSize="3.5rem">
+        {timerMinutes}:{timerSeconds}
+      </Text>
+      <audio
+        // src={"https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3"}
+        controls
+        src={notificationSound}
+        ref={notificationRef}
+      />
+    </Box>
   );
 };
--- redux/TimerSlice.tsx
+++ redux/TimerSlice.tsx
@@ -1,61 +1,82 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const SECONDS_IN_A_MINUTE = 60
+const SECONDS_IN_A_MINUTE = 60;
 
 export interface TimerState {
-  sessionValue: number
-  breakValue: number
-  timerValue : number
-  isRunning : boolean
+  sessionValue: number;
+  breakValue: number;
+  timerValue: number;
+  isRunning: boolean;
+  timerLabel: string;
 }
 
 const initialState: TimerState = {
   sessionValue: 25,
   breakValue: 5,
-  timerValue: 1500,
-  isRunning: false
-}
+  timerValue: 3,
+  isRunning: false,
+  timerLabel: "Session",
+};
 
 const storeState: TimerState = {
   sessionValue: initialState.sessionValue,
   breakValue: initialState.breakValue,
-  timerValue: initialState.sessionValue * SECONDS_IN_A_MINUTE ,
-  isRunning: false
-}
+  timerValue: initialState.sessionValue * SECONDS_IN_A_MINUTE,
+  isRunning: false,
+  timerLabel: "Session",
+};
 
 export const TimerSlice = createSlice({
-  name: 'timer',
+  name: "timer",
   initialState,
   reducers: {
     decrementTimerValue: (state) => {
-      state.timerValue -= 1
+      state.timerValue -= 1;
     },
     updateSession: (state, action: PayloadAction<number>) => {
-      state.sessionValue = action.payload
-      state.timerValue = action.payload * SECONDS_IN_A_MINUTE 
-      storeState.sessionValue = state.sessionValue
-      storeState.timerValue = state.sessionValue * SECONDS_IN_A_MINUTE 
+      state.sessionValue = action.payload;
+      state.timerValue = action.payload * SECONDS_IN_A_MINUTE;
+      storeState.sessionValue = state.sessionValue;
+      storeState.timerValue = state.sessionValue * SECONDS_IN_A_MINUTE;
     },
     updateBreak: (state, action: PayloadAction<number>) => {
-      state.breakValue = action.payload
-      storeState.breakValue = state.breakValue
+      state.breakValue = action.payload;
+      storeState.breakValue = state.breakValue;
     },
     resetTimer: (state) => {
-      state.sessionValue = storeState.sessionValue
-      state.breakValue = storeState.breakValue
-      state.timerValue = storeState.sessionValue * SECONDS_IN_A_MINUTE 
+      state.sessionValue = storeState.sessionValue;
+      state.breakValue = storeState.breakValue;
+      state.timerValue = storeState.sessionValue * SECONDS_IN_A_MINUTE;
     },
     startTimer: (state) => {
-      state.isRunning = true
+      state.isRunning = true;
     },
     stopTimer: (state) => {
-      state.isRunning= false
-    }
-
+      state.isRunning = false;
+    },
+    updateTimerValue: (state, action: PayloadAction<number>) => {
+      state.timerValue = action.payload * SECONDS_IN_A_MINUTE;
+    },
+    toggleLabel: (state, action: PayloadAction<string>) => {
+      if (action.payload === "Session") {
+        state.timerLabel = "Break";
+      } else if (action.payload === "Break") {
+        state.timerLabel = "Session";
+      }
+    },
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { updateSession, updateBreak, resetTimer, decrementTimerValue, startTimer, stopTimer } = TimerSlice.actions
+export const {
+  updateSession,
+  updateBreak,
+  resetTimer,
+  decrementTimerValue,
+  startTimer,
+  stopTimer,
+  updateTimerValue,
+  toggleLabel,
+} = TimerSlice.actions;
 
-export default TimerSlice.reducer
+export default TimerSlice.reducer;
