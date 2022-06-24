4b530be2e1654449c93ae2f40b8bcbc025983829
darrentde <darrent2599@gmail.com>
2022-06-24 22:41:53 +0800

    Fixed module selection within manage todo

--- components/todo/ManageTodo.tsx
+++ components/todo/ManageTodo.tsx
@@ -52,7 +52,7 @@ const ManageTodo = ({
       // setModName((check) => modname.check)(todo.module_id));
       supabase
         .from("modules")
-        .select("code")
+        .select("code, id")
         .eq("id", todo.module_id)
         // .order("id", { ascending: false })
         .then(({ data, error }) => {
