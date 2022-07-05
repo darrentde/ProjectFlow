eb362054a629241cf78a5aabd5bf6820159b213c
darrentde <darrent2599@gmail.com>
2022-07-05 23:04:33 +0800

    Setup jest and react testing library

--- components/timer/TimerSettings.tsx
+++ components/timer/TimerSettings.tsx
@@ -20,44 +20,48 @@ const TimerSettings = () => {
   const dispatch = useDispatch();
 
   return (
-    <Flex flexDirection="column">
-      <Flex flexDirection="row" justifyContent="space-around">
-        <Flex paddingX="-1em"> Pomodoro</Flex>
-        <Flex> Break</Flex>
-      </Flex>
-
-      <Flex flexDirection="row" justifyContent="space-around">
-        <Flex margin="10px">
-          <FormControl>
-            <NumberInput
-              defaultValue={sessionValue}
-              onChange={(value) => dispatch(updateSession(parseInt(value, 10)))}
-              min={1}
-              max={59}
-            >
-              <NumberInputField id="time" />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </FormControl>
+    <Flex data-testid="timer-settings">
+      <Flex flexDirection="column">
+        <Flex flexDirection="row" justifyContent="space-around">
+          <Flex paddingX="-1em"> Pomodoro</Flex>
+          <Flex> Break</Flex>
         </Flex>
-        <Flex margin="10px">
-          <FormControl>
-            <NumberInput
-              defaultValue={breakValue}
-              onChange={(value) => dispatch(updateBreak(parseInt(value, 10)))}
-              min={1}
-              max={59}
-            >
-              <NumberInputField id="break" />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </FormControl>
+
+        <Flex flexDirection="row" justifyContent="space-around">
+          <Flex margin="10px">
+            <FormControl>
+              <NumberInput
+                defaultValue={sessionValue}
+                onChange={(value) =>
+                  dispatch(updateSession(parseInt(value, 10)))
+                }
+                min={1}
+                max={59}
+              >
+                <NumberInputField id="time" />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+          </Flex>
+          <Flex margin="10px">
+            <FormControl>
+              <NumberInput
+                defaultValue={breakValue}
+                onChange={(value) => dispatch(updateBreak(parseInt(value, 10)))}
+                min={1}
+                max={59}
+              >
+                <NumberInputField id="break" />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+          </Flex>
         </Flex>
       </Flex>
     </Flex>
